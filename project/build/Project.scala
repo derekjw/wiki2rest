2fb052f99239e73d07740e2e088460c50810f36f
Derek Williams <derek@nebvin.ca>
2011-04-09 15:12:40 -0600

    Add proguard support for making standalone jar

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,6 +1,16 @@
 import sbt._
 
-class Wiki2reStProject(info: ProjectInfo) extends DefaultProject(info) {
+class Wiki2reStProject(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
   val scalatoolsSnapshots = ScalaToolsSnapshots
   val scalatest = "org.scalatest" % "scalatest" % "1.4-SNAPSHOT" % "test"
+
+  override def proguardOptions = List(
+    "-keepclasseswithmembers public class * { public static void main(java.lang.String[]); }",
+    "-dontoptimize",
+    "-dontobfuscate",
+    proguardKeepLimitedSerializability,
+    proguardKeepAllScala,
+    "-keep interface scala.ScalaObject"
+  )
+  override def proguardInJars = Path.fromFile(scalaLibraryJar) +++ super.proguardInJars
 }
